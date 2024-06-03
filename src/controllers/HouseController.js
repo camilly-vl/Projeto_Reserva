51508c74e1d9f94a3700cd08d5787ef6527c6c5f
CAMILLY <cg3026311@labA204m37.IFSP>
2024-06-03 08:40:57 -0300

    Uptade no HouseController

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -19,7 +19,7 @@ class HouseController {
     const { filename } = req.file
     const { description, price, location, status } = req.body
     const { user_id } = req.headers
-    if(!(await sxhema.isValid(req.body))) {
+    if(!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Falha na validação.'})
     }
     const house = await House.create({
@@ -31,6 +31,36 @@ class HouseController {
       status,
     })
     return res.json(house)
+  }
+
+  async update(req, res) {
+      const schema = Yup.object().shape({
+        description: Yup.string().required(),
+        price: Yup.number().required(),
+        location: Yup.string().required(),
+        status: Yup.boolean().required(),
+      })
+      const { filename } = req.file
+      const { house_id } = req.params
+      const { description, price, location, status } = req.body
+      const { user_id } = req.headers
+      if(!(await schema.isValid(req.body))) {
+        return res.status(400).json({ error: 'Falha na validação.'})
+      }
+      const user = await User.findById(user_id)
+      const houses = await House.findById(house_id)
+      if (String(user._id) !== String(houses.user)) {
+        return res.status(401).json({ error: 'Não autorizado.' }) 
+      }
+      await House.updateOne({ _id: house_id }, {
+        user: user_id,
+        thumbnai: filename,
+        description,
+        price,
+        location,
+        status,
+      })
+      return res.send()
   }
 }
 
